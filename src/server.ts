39333745d7ac369ad9d0f17ef2f9b5cb10dd550e
shaswatag <gshaswata1@gmail.com>
2026-02-28 21:50:42 +0530

    chore: add express bootstrap

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,6 @@
-import express from "express";
+import app from "./app";
+const PORT = process.env.PORT || 5000;
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello from TypeScript + ESM!");
-});
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+})
