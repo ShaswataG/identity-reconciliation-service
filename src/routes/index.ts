d59309836825e8504c9789e48fa4e1fb9a162314
shaswatag <gshaswata1@gmail.com>
2026-03-01 13:42:00 +0530

    feat(contact): register contact router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import contactRouter from "../modules/contact/contact.routes.js";
+
 const router = express.Router();
+
+router.use("/contact", contactRouter);
 
 export default router;
