cf439c4a7a1ffcc830e100611502a72beb4d91c3
shaswatag <gshaswata1@gmail.com>
2026-03-02 12:03:48 +0530

    fix(db): force IPv4 resolution

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,22 @@
 import "dotenv/config";
 import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 const connectionString = process.env.DATABASE_URL;
 
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+  family: 4,
+} as pg.PoolConfig);
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter: new PrismaPg({ connectionString }),
+    adapter: new PrismaPg(pool),
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
